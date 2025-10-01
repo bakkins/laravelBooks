2abee97440d00c7e676a757efaaa2ed95eca93ae
you@example.com <you@example.com>
2025-10-01 11:34:59 +0300

    izvadisana

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Book;
 
 class BookController extends Controller
 {
     public function index()
     {
         $books = Book::all();
-        return view('books')->with('books' , $books);
+        return $books;
     }
 }
